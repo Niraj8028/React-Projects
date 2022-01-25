6c414b3a6c8e8d9f32921416da557b385d24645e
Niraj8028 <nirajpardeshi2002@gmail.com>
2022-01-25 12:45:37 +0530

    React

---
+++ counter/src/App.js
@@ -0,0 +1,14 @@
+import React, {useState} from 'react';
+import "./style.css"
+
+function App() {
+const [count,setCounter]= useState(0);
+  return 
+    <div className='App'>
+    <h1>
+    </h1>
+
+    </div>;
+}
+
+export default App;
