944913be043a7060633583ef717591df8ce20e13
Niraj8028 <nirajpardeshi2002@gmail.com>
2022-02-23 18:38:06 +0530

    basic react projects

--- counter/src/App.js
+++ counter/src/App.js
@@ -2,13 +2,17 @@ import React, {useState} from 'react';
 import "./style.css"
 
 function App() {
-const [count,setCounter]= useState(0);
-  return 
-    <div className='App'>
-    <h1>
-    </h1>
 
-    </div>;
+  const [count,setCount]= useState(0);
+  return (
+    <div>
+    <h1>Counter</h1>
+    <h2>The current count is {count}</h2>
+    <button onClick={()=>setCount(0)}>Reset</button>
+    <button onClick={()=>(count>10 ? "":setCount(count+1))}>Increment</button>
+    <button onClick={()=>(setCount(count-1))}>decrement</button>
+    </div>
+  )
 }
 
 export default App;
