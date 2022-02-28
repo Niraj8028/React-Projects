eb11427f989dfdc45f672be979d87d9412e021d3
Niraj8028 <nirajpardeshi2002@gmail.com>
2022-02-28 20:35:40 +0530

    learning states

--- textUtils/src/components/TextForm.js
+++ textUtils/src/components/TextForm.js
@@ -10,17 +10,34 @@ export default function TextForm(props) {
         console.log("OnChange")
         setText(event.target.value)
     }
-    const [text,setText]=useState('Enter text here');
+    const handleLowerCase=()=>{
+        setText(text.toLowerCase())
+    }
+    const HandleClear=()=>{
+        setText("")
+    }
+    const [text,setText]=useState('');
     return (
-      
-    <div>
+     <>
+    <div className="container">
         <h1>{props.heading}</h1>
         <div className="mb-3">
-            <label for="exampleFormControlTextarea1" className="form-label">Example textarea</label>
+        <h1>Text Converter</h1>
+            <label for="exampleFormControlTextarea1" className="form-label"></label>
             <textarea className="form-control" id="exampleFormControlTextarea1" rows="5" value={text} onChange={handleOnChange}></textarea>
         </div>
-        <button type="button" className="btn btn-dark" onClick={handleClick}>Convert to toUpperCase</button>
+        <button type="button" className="btn btn-dark mx-2" onClick={handleClick}>Convert to toUpperCase</button>
+        <button type="button" className="btn btn-dark mx-2" onClick={handleLowerCase}>Convert to toLowerCase</button>
+        <button type="button" className="btn btn-dark mx-2" onClick={HandleClear}>Clear Text</button>
 
     </div>
+    <div className="container my-3">
+    <h2>Text Summary</h2>
+    Text contains {text.split(" ").length-1} words and {text.length} characters.
+
+    <p className="my-3">{text}</p>
+    </div>
+   
+    </> 
   )
 }
