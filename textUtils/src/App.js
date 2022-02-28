49c12f94eecd53830d633527cefcebe79c1d8a0a
Niraj8028 <nirajpardeshi2002@gmail.com>
2022-02-28 23:21:27 +0530

    project finished

--- textUtils/src/App.js
+++ textUtils/src/App.js
@@ -2,15 +2,20 @@ import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
+import About from './components/About';
 
 function App() {
   return (
+    <>
     <div className="App">
     
       
       <Navbar tittle="Navbar"/>
+      
       <TextForm/>
     </div>
+    <About/>
+    </>
   );
 }
 
