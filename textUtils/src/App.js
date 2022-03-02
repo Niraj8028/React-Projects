eeab038a63e8cdf336313ca0ec72f7516740f81a
Niraj8028 <nirajpardeshi2002@gmail.com>
2022-03-02 18:23:49 +0530

    Using React Routes

--- textUtils/src/App.js
+++ textUtils/src/App.js
@@ -5,29 +5,47 @@ import TextForm from './components/TextForm';
 import About from './components/About';
 import Alerts from './components/alerts';
 import { useState } from 'react';
+import {
+  BrowserRouter,
+  Switch,
+  Route,
+  Routes,
+  Link
+} from "react-router-dom";
 
 function App() {
-  const [alert,setAlert]=useState(null);
-  const showAlert=(Message,type)=>{
+  const [alert, setAlert] = useState(null);
+  const showAlert = (Message, type) => {
     setAlert({
-      msg:Message,
-      type:type
+      msg: Message,
+      type: type
     })
     setTimeout(() => {
       setAlert(null);
     }, 1500);
   }
   return (
-    <>
-    <div className="App">
-    
-      
-      <Navbar tittle="Navbar"/>
-      <Alerts alert={alert}/>
-      <TextForm showAlert={showAlert}/>
+    <div>
+      <Navbar title="Navbar" />
+      <Alerts alert={alert} />
+      <div className="App">
+          <Routes>
+
+
+
+            <Route path="/about" exact element={<About />} />
+
+
+
+            <Route exact path="/" element={<TextForm showAlert={showAlert} />} />
+
+
+
+          </Routes>
+      </div>
     </div>
-    <About showAlert={showAlert} />
-    </>
+
+
   );
 }
 
