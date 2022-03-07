083b7a23d725dea44b749c5d90a25a6e4ac6b148
Niraj Pardeshi <nirajpardeshi2002@gmail.com>
2022-03-07 18:58:28 +0530

    News APi

--- NewApp/src/Components/NewsComponent.js
+++ NewApp/src/Components/NewsComponent.js
@@ -4,16 +4,25 @@ import NewsItem from './NewsItem'
 export default class NewsComponent extends Component {
   render() {
     return (
-      <div>
-      
-        this is a news Component
-        <NewsItem/>
-        <NewsItem/>
-        <NewsItem/>
-        <NewsItem/>
-        <NewsItem/>
-        <NewsItem/>
-        <NewsItem/>
+      <div className='container my-3'>
+
+        <h2>NewsMonkey- Top Headlines</h2>
+        <div className='row'>
+          <div className='col-md-4'>
+            <NewsItem tittle="This is tittle" description="This is description" url="https://images.wsj.net/im-499318/social"/>
+            
+          </div>
+          <div className='col-md-4'>
+            <NewsItem tittle="This is tittle" description="This is description" url="https://images.wsj.net/im-499318/social"/>
+            
+          </div>
+          <div className='col-md-4'>
+            <NewsItem tittle="This is tittle" description="This is description" url="https://images.wsj.net/im-499318/social"/>
+            
+          </div>
+          
+        </div>
+
       </div>
     )
   }
--- NewApp/src/Components/NewsItem.js
+++ NewApp/src/Components/NewsItem.js
@@ -2,8 +2,19 @@ import React, { Component } from 'react'
 
 export class NewsItem extends Component {
   render() {
+    let { tittle, description ,url} = this.props;
     return (
-      <div>NewsItem</div>
+      <div className='container my-3'>
+
+        <div className="card" style={{ width: "18rem" }}>
+          <img src={url} className="card-img-top" alt="..." />
+          <div className="card-body">
+            <h5 className="card-title">{tittle}</h5>
+            <p className="card-text">{description}</p>
+            <a href="#" className="btn btn-primary">Go somewhere</a>
+          </div>
+        </div>
+      </div>
     )
   }
 }
